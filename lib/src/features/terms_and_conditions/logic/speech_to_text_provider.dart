8acc9d5e5a1a08601f3dc4aaf16356f3ce1cb67e
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-24 13:47:05 +0530

    Add SpeechToTextProvider class for speech-to-text functionality

---
+++ lib/src/features/terms_and_conditions/logic/speech_to_text_provider.dart
@@ -0,0 +1,37 @@
+import 'package:ai_app/src/shared/services/speech_to_text/i_speech_to_text_service.dart';
+import 'package:ai_app/src/shared/services/speech_to_text/speech_to_text_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final speechToTextProvider =
+    StateNotifierProvider<SpeechToTextProvider, (String, bool)>(
+  (ref) => SpeechToTextProvider(
+    speechToTextService: ref.read(speechToTextServiceProvider),
+  ),
+);
+
+class SpeechToTextProvider extends StateNotifier<(String, bool)> {
+  SpeechToTextProvider({
+    required ISpeechToTextService speechToTextService,
+  })  : _speechToTextService = speechToTextService,
+        super(('', false));
+
+  final ISpeechToTextService _speechToTextService;
+
+  void updateText(String text) {
+    state = (text, _speechToTextService.isListening);
+  }
+
+  void initSpeech() async {
+    final bool isInitialized = await _speechToTextService.initSpeech();
+    debugPrint('Speech to text initialized: $isInitialized');
+  }
+
+  void startListening() {
+    state = ('', true);
+    _speechToTextService.startListening((result) {
+      debugPrint('result: ${result.recognizedWords}');
+      updateText(result.recognizedWords);
+    });
+  }
+}
